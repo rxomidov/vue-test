b8629a8706aa9a38163ccf7638ce50a7172fd08d
@rixsitilloxomidov <rixsitilloxomidov@gmail.com>
2022-07-17 17:28:16 +0500

    some learnings

---
+++ src/components/MYVueComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello {{ (name, mark) }}</h1>
+</template>
+
+<script>
+export default {
+  name: "MyVueComponent",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    mark: {
+      type: String,
+      default: "!",
+    },
+  },
+};
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,55 @@
+<script setup>
+import MYVueComponent from "../components/MYVueComponent.vue";
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <h1>First test on vue</h1>
+    <MYVueComponent name="World" />
+    <input type="text" v-model="content" />
+    <button class="button" @click="decreaseCount()">decrease</button>
+    <span>{{ count }}</span>
+    <button class="button" @click="increaseCount()">increase</button>
+    <button class="button" @click="save()">SAVE</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      content: "",
+    };
+  },
+  methods: {
+    decreaseCount() {
+      this.count--;
+    },
+    increaseCount() {
+      this.count++;
+    },
+    save() {
+      if (!this.content) {
+        alert("please fill!");
+      }
+      console.log(this.content);
+    },
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
+    display: block;
     align-items: center;
+  }
+  .button {
+    padding: 0.5rem 1rem;
+    margin-left: 1rem;
   }
 }
 </style>
